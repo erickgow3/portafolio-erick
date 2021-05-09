b5548fca6f8c8403ec9e2092a911b57053808ac7
erickgow3 <68794639+erickgow3@users.noreply.github.com>
2021-05-09 01:13:47 -0500

    Add files via upload

---
+++ src/index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('inicio.html')
+
+@app.route('/works')
+def works():
+    return render_template('works.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')    
+
+    if __name__ == '__main__':
+        app.run()
